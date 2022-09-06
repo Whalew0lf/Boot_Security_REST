1019e3419d7ba9f5b686bbda023e611120836b62
Whalew0lf <777chmel777@gmail.com>
2022-09-06 18:20:39 +0300

    Initial commit

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,4 +3,21 @@ $(document).ready(function(){
         $("#currentUser").html(data["email"]);
         $("#currentRoles").html(data["rolesByString"]);
     })
+    $.post("/api/get-all", function (data) {
+       let tbl_body = "";
+        $.each(data, function() {
+            let tbl_row = '<tr class="user' + this["id"] +'">';
+            tbl_row += "<td>"+this["id"]+"</td>";
+            tbl_row += "<td>"+this["firstName"]+"</td>";
+            tbl_row += "<td>"+this["lastName"]+"</td>";
+            tbl_row += "<td>"+this["age"]+"</td>";
+            tbl_row += "<td>"+this["email"]+"</td>";
+            tbl_row += "<td>"+this["rolesByString"]+"</td>";
+            tbl_row += "<td>"+this["email"]+"</td>";
+            tbl_row += "<td>"+this["email"]+"</td>";
+            tbl_body += tbl_row+"</tr>";
+            alert(tbl_row)
+        })
+        $("#usersTable").append(tbl_body);
+    })
 });
--- src/main/resources/static/js/test.js
+++ src/main/resources/static/js/test.js
@@ -2,9 +2,3 @@ $(document).ready(function(){
     $("#id").html("This is Hello World by JQuery");
 
 });
-$("#add").click( function( evnt ) {
-    $.post("/api/get-all",function (data) {
-        $("#id").html(data[1]["email"]);
-    })
-
-});
